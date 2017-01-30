b440a21f35a55512a424d64f2bd51e6f51fe58e8
ewnike <wpltd5@yahoo.com>
2017-01-30 11:46:51 -0600

    assignment

---
+++ site/script.js
@@ -0,0 +1,13 @@
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+
+var arr1= [100,200,300];
+var arr2 = ['TX', 'IL',' MI'];
+var arr3 = ["Hi", "Bye", "Later"];
+
+var boo1 = true;
+var boo2 = false;
+
+var name = {};
